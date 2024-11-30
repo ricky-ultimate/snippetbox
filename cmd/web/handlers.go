46aa466e282673cf8eb25316196e3803ae796059
Ricky Ultimate <rickyogigi@gmail.com>
2024-11-30 17:46:43 +0100

    chore: update base template and home handler to include nav template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,6 +14,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.html",
 		"./ui/html/pages/home.html",
+		"./ui/html/partials/nav.html",
 	}
 
 	ts, err := template.ParseFiles(files...)
